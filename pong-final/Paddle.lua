73e47b2c9d596b5d9c39c15d81b1f18dc2eb9203
Kmiles <71646271+Kdm2633@users.noreply.github.com>
2021-01-29 02:28:57 -0500

    Error Fix

--- pong-final/Paddle.lua
+++ pong-final/Paddle.lua
@@ -59,5 +59,5 @@ end
 ]]
 function Paddle:render()
     love.graphics.rectangle('fill', self.x, self.y, self.width, self.height)
-    love.graphics.setcolor(50/255,89/255,190/255)
+    --love.graphics.setcolor(50/255,89/255,190/255)
 end
